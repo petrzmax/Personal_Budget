decea063480f58d4f7d2c0e75364fa4aee1ae4e1
Artur Petrzak <petrzmax@gmail.com>
2020-10-28 00:24:37 +0100

    Make CMarkup object public

--- XmlFile.h
+++ XmlFile.h
@@ -9,10 +9,13 @@ using namespace std;
 class XmlFile
 {
     const string FILE_NAME;
-    CMarkup xml;
+
 
 public:
     XmlFile(string fileName) : FILE_NAME(fileName){};
+
+    string getFileName();
+    CMarkup xml;
 
 };
 #endif // XMLFILE_H
