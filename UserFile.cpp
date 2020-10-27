a291b2541a921279aa566b34fc4d79fb66110a32
Artur Petrzak <petrzmax@gmail.com>
2020-10-28 00:25:56 +0100

    Add appendUsetToFile method

--- UserFile.cpp
+++ UserFile.cpp
@@ -12,5 +12,25 @@ vector<User> UserFile::loadUsersFromFile()
 
 void UserFile::appendUserToFile(User user)
 {
+    bool fileExists = xml.Load(getFileName());
 
+    if(!fileExists)
+    {
+        xml.SetDoc("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n");
+        xml.AddElem("Users");
+    }
+
+    xml.FindElem(); //Sets position to "Users" element
+    xml.IntoElem(); //Make Users element current parent position
+
+    xml.AddElem("User");
+    xml.IntoElem();
+
+    xml.AddElem("userId", user.getUserId());
+    xml.AddElem("name", user.getName());
+    xml.AddElem("surname", user.getSurname());
+    xml.AddElem("login", user.getLogin());
+    xml.AddElem("password", user.getPassword());
+
+    xml.Save(getFileName());
 }
