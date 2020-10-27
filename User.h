f10fc00b367a87a1b8fbfe5c4176cb9e3ec68ca7
Artur Petrzak <petrzmax@gmail.com>
2020-10-27 19:42:05 +0100

    Add constructor to User class

--- User.h
+++ User.h
@@ -14,6 +14,15 @@ class User
     string password;
 
 public:
+    User(int userId = 0, string name = "", string surname = "", string login = "", string password = "")
+    {
+        this->userId = userId;
+        this->name = name;
+        this->surname = surname;
+        this->login = login;
+        this->password = password;
+    }
+
     void setUserId(int userId);
     void setName(string name);
     void setSurname(string surname);
