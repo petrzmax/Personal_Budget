600ad772348d6268aeab16d5901b61f66061cf31
Artur Petrzak <petrzmax@gmail.com>
2020-10-27 20:28:19 +0100

    Make Income class inherit from Finance class

--- Income.h
+++ Income.h
@@ -2,37 +2,23 @@
 #define INCOME_H
 
 #include <iostream>
+#include "Finance.h"
 
 using namespace std;
 
-class Income
+class Income : public Finance
 {
     int incomeId;
-    int userId;
-    time_t date;
-    string item;
-    float amount;
 
 public:
-    Income(int incomeId = 0, int userId = 0, time_t date = 0, string item = "", float amount = 0)
+    Income(int incomeId = 0, int userId = 0, time_t date = 0, string item = "", float amount = 0) :
+        Finance(userId, date, item, amount)
     {
         this->incomeId = incomeId;
-        this->userId = userId;
-        this->date = date;
-        this->item = item;
-        this->amount = amount;
     }
 
     void setIncomeId(int incomeId);
-    void setUserId(int userId);
-    void setDate(time_t date);
-    void setItem(string item);
-    void setAmount(float amount);
 
     int getIncomeId();
-    int getUserId();
-    time_t getDate();
-    string getItem();
-    float getAmount();
 };
 #endif // INCOME_H
