5c7af17cb4789ad29a2065e2024640ce066580eb
Artur Petrzak <petrzmax@gmail.com>
2020-10-28 14:21:59 +0100

    Create UserManager class prototype

---
+++ UserManager.cpp
@@ -0,0 +1,41 @@
+#include "UserManager.h"
+
+User UserManager::getNewUserData()
+{
+
+}
+
+int UserManager::getNewUserId()
+{
+
+}
+
+bool UserManager::loginExist()
+{
+
+}
+
+void UserManager::registerUser()
+{
+
+}
+
+void UserManager::loginUser()
+{
+
+}
+
+void UserManager::logoutUser()
+{
+
+}
+
+void UserManager::changePassword()
+{
+
+}
+
+bool UserManager::isUserLoggedIn()
+{
+
+}
---
+++ UserManager.h
@@ -0,0 +1,34 @@
+#ifndef USERMANAGER_H
+#define USERMANAGER_H
+
+#include "UserFile.h"
+
+using namespace std;
+
+class UserManager
+{
+    int loggedUserId;
+    vector <User> users;
+    UserFile userFile;
+
+    User getNewUserData();
+    int getNewUserId();
+    bool loginExist();
+
+public:
+
+    UserManager(string usersFileName) : userFile(usersFileName)
+    {
+        users = userFile.loadUsersFromFile();
+    }
+
+    void registerUser();
+    void loginUser();
+    void logoutUser();
+    void changePassword();
+
+    bool isUserLoggedIn();
+
+};
+
+#endif // USERMANAGER_H
